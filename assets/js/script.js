1a237121ce605d384a4ac5cf0792d024ef6d36cc
soni <sonisah273b@gmail.com>
2025-03-22 18:17:24 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// Change navbar background on scroll
+window.addEventListener("scroll", function() {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("solid");
+    } else {
+        navbar.classList.remove("solid");
+    }
+});
+
+
+// card
+
+
+         
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+    
